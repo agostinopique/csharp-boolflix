86162481070932f148fe329b212a988ad8b51d44
agostinopique <agostino.pique@gmail.com>
2022-10-11 17:56:44 +0200

    Aggiungere i file di progetto.

---
+++ Models/Context/BoolflixDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace csharp_boolflix.Models.Context
+{
+    public class BoolflixDbContext : DbContext 
+    {
+        public DbSet<Film> Films { get; set; }
+        public DbSet<TVSeries> TvSeries { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<MediaInfo> MediaInfos { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=boolflix-db;Integrated Security=True");
+        }
+    }
+}
